2c02115f3139a097010a910d80f814c56008a0e9
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-01-19 01:19:12 +0900

    display dates and contents

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express();
 
+app.use('/public', express.static('public'))
+
+
 app.get('/', (req, res) => {
-    res.render('hello.ejs');
+    res.render('top.ejs');
 });
 
+app.get('/index', (req, res) => {
+    res.render('index.ejs')
+})
 app.listen(3000);
